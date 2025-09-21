b309f5a7807b3ad111cf8b3c13fd0f7aea24edf1
sorinservice <nemesisofnocturn@gmail.com>
2025-09-22 01:57:48 +0200

    Update manager.lua

--- main/current-game/manager.lua
+++ main/current-game/manager.lua
@@ -2,15 +2,15 @@
 -- Map your games here. Prefer UniverseId (game.GameId); fall back to PlaceId.
 return {
     byUniverse = {
-        [2992873140] = { -- <== UniverseId
+        [2992873140] = {  -- Emergency Hamburg (UniverseId)
             name   = "Emergency Hamburg",
-            module = "https://raw.githubusercontent.com/sorinservice/60bbda1f-8e4d-4eb3-ad7f-dcd212800a14/refs/heads/main/main/current-game/games/EmergencyHamburg.lua",
+            module = "https://raw.githubusercontent.com/sorinservice/60bbda1f-8e4d-4eb3-ad7f-dcd212800a14/main/current-game/games/EmergencyHamburg.lua",
         },
-    },
-    byUniverse = {
-        [0000000000] = { -- <== UniverseId
+
+        -- Beispiel weiterer Titel:
+        [0] = {  -- <== ersetze 0 durch echte UniverseId
             name   = "Game",
-            module = "https://raw.githubusercontent.com/sorinservice/60bbda1f-8e4d-4eb3-ad7f-dcd212800a14/refs/heads/main/main/current-game/games/Game",
+            module = "https://raw.githubusercontent.com/sorinservice/60bbda1f-8e4d-4eb3-ad7f-dcd212800a14/main/current-game/games/Game.lua",
         },
     },
 }
