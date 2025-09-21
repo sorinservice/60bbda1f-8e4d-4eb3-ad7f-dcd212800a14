78749405d59a2bb215bf7f198559b8d6279d0eca
sorinservice <nemesisofnocturn@gmail.com>
2025-09-22 00:37:32 +0200

    Create Developer.lua

---
+++ main/Developer.lua
@@ -0,0 +1,50 @@
+-- Developer.lua
+return function(Tab, Luna, Window)
+    local HttpService = game:GetService("HttpService")
+    local Players = game:GetService("Players")
+
+    local function copyAndNotify(label, value)
+        setclipboard(tostring(value))
+        Luna:Notification({
+            Title = "Copied",
+            Icon = "check",
+            ImageSource = "Material",
+            Content = label .. " copied to clipboard: " .. tostring(value)
+        })
+    end
+
+    Tab:CreateSection("Game Information")
+
+    Tab:CreateButton({
+        Name = "Copy PlaceId",
+        Description = "The unique ID of this place",
+        Callback = function()
+            copyAndNotify("PlaceId", game.PlaceId)
+        end
+    })
+
+    Tab:CreateButton({
+        Name = "Copy GameId",
+        Description = "The persistent ID of this game",
+        Callback = function()
+            copyAndNotify("GameId", game.GameId)
+        end
+    })
+
+    Tab:CreateButton({
+        Name = "Copy Game Name",
+        Description = "The name of this game",
+        Callback = function()
+            copyAndNotify("Name", game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name)
+        end
+    })
+
+    Tab:CreateButton({
+        Name = "Copy CreatorId",
+        Description = "The ID of the creator (user/group)",
+        Callback = function()
+            local info = game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId)
+            copyAndNotify("CreatorId", info.Creator.CreatorTargetId)
+        end
+    })
+end
