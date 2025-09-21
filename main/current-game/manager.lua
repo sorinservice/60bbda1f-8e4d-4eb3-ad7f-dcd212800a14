afbe5f874a92dfca5a17a8b57e1aae2f0734fac5
sorinservice <nemesisofnocturn@gmail.com>
2025-09-22 01:59:03 +0200

    Update manager.lua

--- main/current-game/manager.lua
+++ main/current-game/manager.lua
@@ -4,7 +4,7 @@ return {
     byUniverse = {
         [2992873140] = {  -- Emergency Hamburg (UniverseId)
             name   = "Emergency Hamburg",
-            module = "https://raw.githubusercontent.com/sorinservice/60bbda1f-8e4d-4eb3-ad7f-dcd212800a14/main/current-game/games/EmergencyHamburg.lua",
+            module = "https://raw.githubusercontent.com/sorinservice/60bbda1f-8e4d-4eb3-ad7f-dcd212800a14/refs/heads/main/main/current-game/games/EmergencyHamburg.lua",
         },
 
         -- Beispiel weiterer Titel:
