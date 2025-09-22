5235c36772f98f1faf69618a64f9aaa08ef109f9
sorinservice <nemesisofnocturn@gmail.com>
2025-09-22 10:41:21 +0200

    Update manager.lua

--- main/current-game/manager.lua
+++ main/current-game/manager.lua
@@ -7,10 +7,10 @@ return {
             module = "https://raw.githubusercontent.com/sorinservice/60bbda1f-8e4d-4eb3-ad7f-dcd212800a14/main/main/current-game/games/EmergencyHamburg.lua",
         },
 
-        -- weitere Spiele:
-        -- [1234567890] = {
-        --     name   = "My Other Game",
-        --     module = "https://raw.githubusercontent.com/.../current-game/games/MyOtherGame.lua",
-        -- },
+
+        -[5829141886] = {
+             name   = "RealisticCarDriving.lua",
+             module = "https://raw.githubusercontent.com/sorinservice/60bbda1f-8e4d-4eb3-ad7f-dcd212800a14/main/main/current-game/games/RealisticCarDriving.lua",
+         },
     },
 }
